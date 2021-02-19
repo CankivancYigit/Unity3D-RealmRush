d861aaf74c6d71772ac6003fda8e630d01e5fd25
CankivancYigit <CankivancYigit@github.com>
2021-02-20 02:25:52 +0300

    EditorSnap Script Name Has Been Changed To CubeEditor And Labels Added To Cube For Coordinates.

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
     void Update()
     {
         Vector3 snapPos;
@@ -13,5 +15,8 @@ public class EditorSnap : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
