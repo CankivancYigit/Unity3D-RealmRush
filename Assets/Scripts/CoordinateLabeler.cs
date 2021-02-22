3d2363ff4b6c861baaa6ef788da366038bf74a8e
CankivancYigit <CankivancYigit@github.com>
2021-02-22 05:12:33 +0300

    Required Assets Are Imported. Cube Changed To Tile. Cube Editor Script Name Changed To CoordinateLabeler. Waypoints And Enemy Moving System Added.

---
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMesh textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        DisplayCoordinates();
+    }
+
+    private void DisplayCoordinates()
+    {
+        Vector3 coordinates;
+        coordinates.x = Mathf.RoundToInt(transform.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.z = Mathf.RoundToInt(transform.position.z / UnityEditor.EditorSnapSettings.move.z);
+        string labelText = coordinates.x  + "," + coordinates.z;
+
+        textMesh.text = labelText;    
+        gameObject.name = labelText;    //Updates Object Name To Coordinates
+    }
+}
